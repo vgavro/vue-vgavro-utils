989df754e287a0dc0bf776a6914d34c427239eae
Victor Gavro <vgavro@gmail.com>
2017-12-09 07:23:44 +0200

    utils: added element visibility

--- utils.js
+++ utils.js
@@ -135,5 +135,16 @@ export function geolocationPromise () {
   })
 }
 
+export function isElementInViewport (el) {
+  // see https://stackoverflow.com/a/7557433/450103
+  var rect = el.getBoundingClientRect()
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  )
+}
+
 export const EMAIL_REGEXP = /\S+@\S+\.\S+/
 export const PHONE_REGEXP = /\+[1-9]{1}[0-9]{3,14}/
