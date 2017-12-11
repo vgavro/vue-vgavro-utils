8e4416509718dd38822def75173a60923b387c16
Victor Gavro <vgavro@gmail.com>
2017-12-11 20:37:28 +0200

    RequestsMixin: request may be used as direct function call, removed payload parameter

--- mixins/RequestsMixin.js
+++ mixins/RequestsMixin.js
@@ -1,46 +1,35 @@
-import { mapValues } from 'lodash'
-import { injectComponentOptionsData } from '../utils'
+import Vue from 'vue'
 
 function createRequest (vm, name, request) {
-  return {
-    inProgress: 0,
-    payload: null,
-    error: null,
-    data: null,
-
-    call (payload) {
-      const result = request.call(vm, payload)
-      if (result) {
-        const self = vm[name]
-
-        self.inProgress += 1
-        self.error = null
-        self.payload = payload
-
-        return result
-          .then(data => {
-            self.inProgress -= 1
-            self.data = data
-            self.error = null
-            return data
-          })
-
-          .catch(error => {
-            self.inProgress -= 1
-            self.data = null
-            self.error = error
-            return Promise.reject(error)
-          })
-      }
-    },
+  const request_ = function () {
+    const result = request.apply(vm, arguments)
+    if (!result) return
+    request_.loading += 1
+    request_.error = null
+    return result
+      .then(data => {
+        request_.loading -= 1
+        request_.data = data
+        request_.error = null
+        return data
+      })
+      .catch(error => {
+        request_.loading -= 1
+        request_.data = null
+        request_.error = error || true // empty reject? shame on you!
+        return Promise.reject(error)
+      })
   }
+  Vue.util.defineReactive(request_, 'loading', 0)
+  Vue.util.defineReactive(request_, 'data', null)
+  Vue.util.defineReactive(request_, 'error', null)
+  return request_
 }
 
 export default {
   beforeCreate () {
-    injectComponentOptionsData(this, mapValues(
-      this.$options.requests,
-      (value, key) => createRequest(this, key, value)
-    ))
+    Object.entries(this.$options.requests).forEach(([key, value]) => {
+      this[key] = createRequest(this, key, value)
+    })
   },
 }
