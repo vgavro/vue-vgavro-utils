97855ba6b5e94e235bc3e412e3d02687fb45a041
Victor Gavro <vgavro@gmail.com>
2018-07-25 16:53:35 +0300

    utils.deepMerge added

--- utils.js
+++ utils.js
@@ -20,6 +20,29 @@ export function deepCopy (obj) {
   return JSON.parse(JSON.stringify(obj))
 }
 
+export function deepMerge (...objects) {
+  // Also concatenates arrays in object keys
+  // https://stackoverflow.com/a/48218209/450103
+  const isObject = obj => obj && typeof obj === 'object'
+
+  return objects.reduce((prev, obj) => {
+    Object.keys(obj).forEach(key => {
+      const pVal = prev[key]
+      const oVal = obj[key]
+
+      if (Array.isArray(pVal) && Array.isArray(oVal)) {
+        prev[key] = pVal.concat(...oVal)
+      } else if (isObject(pVal) && isObject(oVal)) {
+        prev[key] = deepMerge(pVal, oVal)
+      } else {
+        prev[key] = oVal
+      }
+    })
+
+    return prev
+  }, {})
+}
+
 export function timeoutPromise (time, value) {
   let timeout = null
 
